4be6b199e2f1b8c7f504d525e5738c11a3d6cdae
Kunal Rathod <kunal.rathod@majority.com>
2026-09-22 22:26:07 +0200

    Initial Commit

---
+++ iOSCodingTask/iOSCodingTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCodingTaskApp.swift
+//  iOSCodingTask
+//
+//  Created by Kunal Rathod on 2026-09-22.
+//
+
+import SwiftUI
+
+@main
+struct iOSCodingTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
